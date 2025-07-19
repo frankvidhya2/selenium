f7cf0da53d433db5b75bcd1a3c85a2dbe12a5e38
frankvidhya2 <vidhyashreeravi93@gmail.com>
2025-07-19 20:27:24 +0530

    Add files via upload
    javaoprators

---
+++ javaoperators/chrome.java
@@ -0,0 +1,22 @@
+package javaoperators;
+
+public class chrome {
+
+	public static void main(String[] args) {
+		  // Declaring and initializing variables of different data types
+		    float version = 91.0f;
+	        String developer = "Google";
+	        boolean isBeta = false;
+	        int releaseYear = 2008;
+	        char shortcutKey = 'C';
+
+	       // Printing the values
+	        System.out.println(version);
+	        System.out.println(developer);
+	        System.out.println(isBeta);
+	        System.out.println(releaseYear);
+	        System.out.println(shortcutKey);
+
+	}
+
+}
